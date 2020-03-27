b88aecbeec36a5061d73b74df47dec3292ca3b65
Junsoo Park <junsoo.feb@gmail.com>
2020-03-27 14:27:04 +0900

    update

--- main.py
+++ main.py
@@ -30,24 +30,38 @@ def save_state(model, best_acc):
     torch.save(state, 'models/nin.pth.tar')
 
 def train(epoch):
-    model.train()
+    # model은 nin.py의 Net 클래스! Net클래스는 nn.module 을 상속
+    # nn.module의 train()메소드
+    model.train() # 모듈을 훈련 모드로 설정.
+
     for batch_idx, (data, target) in enumerate(trainloader):
-        # process the weights including binarization
-        bin_op.binarization()
+         # util.py의 BinOp 클래스
+        bin_op.binarization() # weight를 binarization 처리
         
-        # 순전파
+        # <순전파>
+        # torch.autograd.variable
+        # autograd를 사용하면 역전파때 필요한 미분 값을 자동으로 계산
         data, target = Variable(data.cuda()), Variable(target.cuda())
+        
+        # 역전파 전에, 갱신할 변수들에 대한 모든 기울기를 0으로 만든다
+        # 이유는 backward()를 호출할 때마다 기울기가 누적되기 때문
         optimizer.zero_grad()
+        
+        # 모델에 data를 전달하여 예상되는 output을 계산한다
         output = model(data)
         
-        # 역전파
+        # <역전파>
+        # criterion 은 nn.CrossEntropyLoss()
+        # output과 실제 target에 대한 loss를 구한다
         loss = criterion(output, target)
+        # 미분값 계산
         loss.backward()
         
         # 파라미터 저장
         bin_op.restore()
         bin_op.updateBinaryGradWeight()
         
+        # 기울기 업데이트
         optimizer.step()
         if batch_idx % 100 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tLR: {}'.format(
@@ -58,7 +72,7 @@ def train(epoch):
 
 def test():
     global best_acc
-    model.eval()
+    model.eval() # 모듈을 평가모드로 설정
     test_loss = 0
     correct = 0
     bin_op.binarization()
@@ -124,13 +138,13 @@ if __name__=='__main__':
         raise Exception\
                 ('Please assign the correct data path with --data <DATA_PATH>')
 
-    # data.py의 dataset클래스 생성
+    # data.py의 dataset클래스 생성, DataLoader == Dataset을 인자로 받아 data를 뽑아냄
     trainset = data.dataset(root=args.data, train=True)
     # torch.utils.data.DataLoader는 불러온 data를 네트워크 입력으로 사용하기 위해 사전에 정리를 해주는 느낌
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
             shuffle=True, num_workers=2)
 
-    # data.py의 dataset클래스 생성
+    # data.py의 dataset클래스 생성, DataLoader ==  Dataset을 인자로 받아 data를 뽑아냄
     testset = data.dataset(root=args.data, train=False)
     # torch.utils.data.DataLoader는 불러온 data를 네트워크 입력으로 사용하기 위해 사전에 정리를 해주는 느낌
     testloader = torch.utils.data.DataLoader(testset, batch_size=100,
@@ -200,6 +214,34 @@ if __name__=='__main__':
 
     # 학습 시작하는 부분
     for epoch in range(1, 320):
-        adjust_learning_rate(optimizer, epoch)
+        adjust_learning_rate(optimizer, epoch) # 지정된 epoch마다 lr = lr * 0.1 적용
+
         train(epoch)
+        
+        '''
+        < train 과정 >
+        
+        1. bin_op.binarization() 실행
+
+        binarization()은 아래의 4개 메소드로 구성         
+         self.meancenterConvParams() == 모듈이 Conv2d일때, Conv2d.weight의 평균값을 구함
+         self.clampConvParams() == Conv2d.weight가 -1.0 ~ 1.0 사이의 값이 되도록 조절 
+         self.save_params() == Conv2d.weight저장
+         self.binarizeConvParams() == Conv2d.weight를 -1 또는 1로 binarize
+
+
+        2. 순전파
+
+         모델에 data를 전달하여 예상되는 output을 계산한다
+
+        3. 역전파
+        
+         output과 실제 target에 대한 loss를 구한다
+
+        4. loss가 최소가 되는 방향으로 parameter와 grad 업데이트
+
+        5. 반복
+
+
+        '''
         test()
--- util.py
+++ util.py
@@ -1,40 +1,57 @@
 import torch.nn as nn
 import numpy
 
+# model 은 nin.py의 Net클래스객체
+
 class BinOp():
     def __init__(self, model):
-        # count the number of Conv2d
         count_Conv2d = 0
+        # Net에 있는 conv2d의 개수 카운트
         for m in model.modules():
+            # 모듈 m이 conv2d라면
             if isinstance(m, nn.Conv2d):
                 count_Conv2d = count_Conv2d + 1
 
         start_range = 1
         end_range = count_Conv2d-2
+
+        # <bin_range> 
+        # start_range부터 end_range까지 end_range-start_range+1 개의 균일한 간격의 정수들로 이루어진 array
         self.bin_range = numpy.linspace(start_range,
                 end_range, end_range-start_range+1)\
                         .astype('int').tolist()
+    
+        # num_of_params 는 bin_range의 원소의 개수,  end_range-start_range+1 과 같음
         self.num_of_params = len(self.bin_range)
         self.saved_params = []
         self.target_params = []
         self.target_modules = []
         index = -1
+
+        # Net에 있는 모듈에 대해서 수행
         for m in model.modules():
+            # 모듈 m이 conv2d라면
             if isinstance(m, nn.Conv2d):
                 index = index + 1
+                # index가 bin_range에 포함된다면
                 if index in self.bin_range:
+                    # Conv2d.weight.data 복사 후 tmp에 저장
+                    # Conv2d.weight.data 와 Conv2d.weight 의 차이 알아보기!
                     tmp = m.weight.data.clone()
                     self.saved_params.append(tmp)
                     self.target_modules.append(m.weight)
 
     def binarization(self):
-        self.meancenterConvParams()
-        self.clampConvParams()
-        self.save_params()
-        self.binarizeConvParams()
+        self.meancenterConvParams()  # 모듈이 Conv2d일때, Conv2d.weight의 평균값을 구함
+        self.clampConvParams() # weight가 -1.0 ~ 1.0 사이의 값이 되도록 조절 
+        self.save_params() # Conv2d.weight저장
+        self.binarizeConvParams() # Conv2d.weight를 -1 또는 1로 binarize
+
+
 
     def meancenterConvParams(self):
         for index in range(self.num_of_params):
+            # target_modules는 nn.Conv2d.weight로 구성된 리스트
             s = self.target_modules[index].data.size()
             negMean = self.target_modules[index].data.mean(1, keepdim=True).\
                     mul(-1).expand_as(self.target_modules[index].data)
@@ -42,6 +59,7 @@ class BinOp():
 
     def clampConvParams(self):
         for index in range(self.num_of_params):
+            # target_modules는 nn.Conv2d.weight로 구성된 리스트
             self.target_modules[index].data = \
                     self.target_modules[index].data.clamp(-1.0, 1.0)
 
