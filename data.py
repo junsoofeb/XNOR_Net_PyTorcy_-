fdc19cb97a48c51664bdaa9397f53070c11fe9b9
Junsoo Park <junsoo.feb@gmail.com>
2020-04-01 20:36:43 +0900

    Update data.py

--- data.py
+++ data.py
@@ -10,13 +10,24 @@ class dataset():
     def __init__(self, root=None, train=True):
         self.root = root
         self.train = train
-        self.transform = transforms.ToTensor()
+        self.transform = transforms.ToTensor()  # transforms.ToTensor() --> PIL 이미지 또는 넘파이배열을 pytorch의 텐서로 변형
+        
+        # train 일때
         if self.train:
-            train_data_path = os.path.join(root, 'train_data')
+            train_data_path = os.path.join(root, 'train_data') # os.path.join(root, 'train_data') --> 경로 병합 "{root}/train_data"
             train_labels_path = os.path.join(root, 'train_labels')
-            self.train_data = numpy.load(open(train_data_path, 'rb'))
-            self.train_data = torch.from_numpy(self.train_data.astype('float32'))
+            
+            # open(train_data_path, 'rb') --> 경로에서 파일 읽기, cf) 파일경로 : train_data_path, 모드 : rb (read binary)
+            # numpy.load() --> 저장된 넘파이배열 불러오기
+            self.train_data = numpy.load(open(train_data_path, 'rb')) 
+            
+            # torch.from_numpy() --> 넘파이배열을 텐서로 변형
+            # 원래의 ndarray 객체를 참조하므로 원래 ndarray 객체의 값을 바꾸면 텐서 자료형의 값도 바뀌고 반대도 마찬가지
+            self.train_data = torch.from_numpy(self.train_data.astype('float32')) 
+            
             self.train_labels = numpy.load(open(train_labels_path, 'rb')).astype('int')
+        
+        # inference 일때
         else:
             test_data_path = os.path.join(root, 'test_data')
             test_labels_path = os.path.join(root, 'test_labels')
@@ -24,13 +35,14 @@ class dataset():
             self.test_data = torch.from_numpy(self.test_data.astype('float32'))
             self.test_labels = numpy.load(open(test_labels_path, 'rb')).astype('int')
 
-    def __len__(self):
+    # 매직 메소드
+    def __len__(self): # 데이터의 개수 반환
         if self.train:
             return len(self.train_data)
         else:
             return len(self.test_data)
 
-    def __getitem__(self, index):
+    def __getitem__(self, index): # img와 label 반환
         if self.train:
             img, target = self.train_data[index], self.train_labels[index]
         else:
