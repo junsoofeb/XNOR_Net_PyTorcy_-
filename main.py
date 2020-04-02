9942c9cb615ac62e22a5960c897c9c165654c5e8
Junsoo Park <junsoo.feb@gmail.com>
2020-04-02 15:02:30 +0900

    update

--- main.py
+++ main.py
@@ -18,15 +18,19 @@ from torch.autograd import Variable
 
 def save_state(model, best_acc):
     print('==> Saving model ...')
-    # 모델과 정확도 저장
+    '''
+    model과 accuracy 저장
+    
+    model은 nin.py의 Net클래스이며, nn.module 을 상속
+    model.state_dict() --> module 전체의 state를 dict형태로 반환
+    '''
     state = {
             'best_acc': best_acc,
             'state_dict': model.state_dict(),
             }
     for key in state['state_dict'].keys():
         if 'module' in key:
-            state['state_dict'][key.replace('module.', '')] = \
-                    state['state_dict'].pop(key)
+            state['state_dict'][key.replace('module.', '')] = state['state_dict'].pop(key)
     torch.save(state, 'models/nin.pth.tar')
 
 def train(epoch):
@@ -34,17 +38,18 @@ def train(epoch):
     # nn.module의 train()메소드
     model.train() # 모듈을 훈련 모드로 설정.
 
+    # enumerate(trainloader) : ( batch_index, (data, target) ) 으로  구성
     for batch_idx, (data, target) in enumerate(trainloader):
-         # util.py의 BinOp 클래스
+        # util.py의 BinOp 클래스
         bin_op.binarization() # weight를 binarization 처리
         
         # <순전파>
-        # torch.autograd.variable
+        # Variable == torch.autograd.variable
         # autograd를 사용하면 역전파때 필요한 미분 값을 자동으로 계산
         data, target = Variable(data.cuda()), Variable(target.cuda())
         
         # 역전파 전에, 갱신할 변수들에 대한 모든 기울기를 0으로 만든다
-        # 이유는 backward()를 호출할 때마다 기울기가 누적되기 때문
+        # 이유 : backward()를 호출할 때마다 기울기가 누적되기 때문
         optimizer.zero_grad()
         
         # 모델에 data를 전달하여 예상되는 output을 계산한다
@@ -98,6 +103,7 @@ def test():
     return
 
 def adjust_learning_rate(optimizer, epoch):
+# 지정된 epoch에 해당하면, optimizer의 lr을 0.1 곱해준다. 
     update_list = [120, 200, 240, 280]
     if epoch in update_list:
         for param_group in optimizer.param_groups:
@@ -111,44 +117,44 @@ def adjust_learning_rate(optimizer, epoch):
 
 
 if __name__=='__main__':
-    # 옵션 주는 부분
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--cpu', action='store_true',
-            help='set if only CPU is available')
-    parser.add_argument('--data', action='store', default='./data/',
-            help='dataset path')
-    parser.add_argument('--arch', action='store', default='nin',
-            help='the architecture for the network: nin')
-    parser.add_argument('--lr', action='store', default='0.01',
-            help='the intial learning rate')
-    parser.add_argument('--pretrained', action='store', default=None,
-            help='the path to the pretrained model')
-    parser.add_argument('--evaluate', action='store_true',
-            help='evaluate the model')
+    
+    parser = argparse.ArgumentParser() # 인자 받을 수 있도록 ArgumentPars() 객체 생성
+
+    # 입력받을 인자값 등록 
+    parser.add_argument('--cpu', action='store_true', help='set if only CPU is available')
+    parser.add_argument('--data', action='store', default='./data/', help='dataset path')
+    parser.add_argument('--arch', action='store', default='nin', help='the architecture for the network: nin')
+    parser.add_argument('--lr', action='store', default='0.01', help='the intial learning rate')
+    parser.add_argument('--pretrained', action='store', default=None, help='the path to the pretrained model')
+    parser.add_argument('--evaluate', action='store_true', help='evaluate the model')
+    
+    # command line에서 입력받은 인자들 파싱 후 args에 저장 
     args = parser.parse_args()
     print('==> Options:',args)
+    # print(args.cpu)
+    # print(args.data) 등으로 입력받은 인자 출력 가능
 
-    # 랜덤 값으로 채워진 텐서 생성을 위해 seed 설정
-    torch.manual_seed(1)
-    torch.cuda.manual_seed(1)
 
-    # 데이터 경로 확인
+    # 랜덤 값으로 채워진 텐서 생성을 위해 seed 설정 --> 항상 같은 결과가 나오게 됨.
+    torch.manual_seed(1) # cpu 연산때 무작위성 배제하기 위해서
+    torch.cuda.manual_seed(1) # gpu 연산때 무작위성 배제하기 위해서
+
+    # train_data가 인자로 입력한 경로에 존재하는지 확인
     if not os.path.isfile(args.data+'/train_data'):
-        # check the data path
         raise Exception\
                 ('Please assign the correct data path with --data <DATA_PATH>')
 
-    # data.py의 dataset클래스 생성, DataLoader == Dataset을 인자로 받아 data를 뽑아냄
+    # data.py의 dataset클래스 생성, DataLoader --> Dataset을 인자로 받아 data를 뽑아냄
     trainset = data.dataset(root=args.data, train=True)
     # torch.utils.data.DataLoader는 불러온 data를 네트워크 입력으로 사용하기 위해 사전에 정리를 해주는 느낌
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
-            shuffle=True, num_workers=2)
+    # trainloader는 (train_data, target)으로 구성
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
-    # data.py의 dataset클래스 생성, DataLoader ==  Dataset을 인자로 받아 data를 뽑아냄
+    # data.py의 dataset클래스 생성, DataLoader -->  Dataset을 인자로 받아 data를 뽑아냄
     testset = data.dataset(root=args.data, train=False)
     # torch.utils.data.DataLoader는 불러온 data를 네트워크 입력으로 사용하기 위해 사전에 정리를 해주는 느낌
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100,
-            shuffle=False, num_workers=2)
+    # testloader는 (test_data, target)으로 구성
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
     # 클래스 정의
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
@@ -166,10 +172,12 @@ if __name__=='__main__':
     if not args.pretrained:
         print('==> Initializing model parameters ...')
         best_acc = 0
+        # model.modules() --> 네트워크 속 모든 모듈을 반복가능한 형태(iter)로 반환
         for m in model.modules():
+            # m이 nn.Conv2d()의 인스턴스라면,
             if isinstance(m, nn.Conv2d):
-                m.weight.data.normal_(0, 0.05)
-                m.bias.data.zero_()
+                m.weight.data.normal_(0, 0.05) # weigt는 평균은 0 표준편차 0.05를 따르는 정규분포
+                m.bias.data.zero_() # bias는 0으로 설정
                 
     # 옵션에 pretrained model을 사용하는 경우
     else:
@@ -191,16 +199,35 @@ if __name__=='__main__':
     print(model)
 
 
-    # learning rate, optimizer등 설정
+    # base_lr은 인자로 받은 lr로 설정, default : 0.01
     base_lr = float(args.lr)
+    # nn.Module.named_parameters() --> param 자체와 param의 이름 둘 다 가지고 있는 반복가능한 객체
+    # 반복가능한 객체를 dict로 변환.  
     param_dict = dict(model.named_parameters())
     params = []
 
+    # params 이라는 리스트는 아래와같은 형태
+    #  [
+    #    {'params':[value0], 'lr': base_lr, 'weight_decay':0.00001},
+    #    {'params':[value1], 'lr': base_lr, 'weight_decay':0.00001}, 
+    #    {'params':[value2], 'lr': base_lr, 'weight_decay':0.00001}, 
+    #     ...
+    #    {'params':[valueN], 'lr': base_lr, 'weight_decay':0.00001}  
+    #  ]
     for key, value in param_dict.items():
-        params += [{'params':[value], 'lr': base_lr,
-            'weight_decay':0.00001}]
+        params += [{'params':[value], 'lr': base_lr, 'weight_decay':0.00001}]
 
-    optimizer = optim.Adam(params, lr=0.10,weight_decay=0.00001)
+
+    '''
+    Adam --> Momentum과 AdaGrad를 섞은 기법
+    경사하강법 단점 --> 무작정 기울어진 방향으로 이동하기 때문에 탐색경로가 비효율적이어서 시간이 오래걸린다.
+    Momentum --> 속도(운동량)가 크게 나올수록 기울기가 크게 업데이트 되어 경사하강법이 가지는 단점을 보완가능.
+    AdaGrad --> 학습을 진행하면서 점차 학습률을 줄여나가는 방법.
+    '''
+
+    # Adam 알고리즘을 구현한 optimizer
+    optimizer = optim.Adam(params, lr=0.10, weight_decay=0.00001)
+    # 교차 엔트로피 손실함수 설정, 클래스 분류에 적절
     criterion = nn.CrossEntropyLoss()
 
 
@@ -214,7 +241,7 @@ if __name__=='__main__':
 
     # 학습 시작하는 부분
     for epoch in range(1, 320):
-        adjust_learning_rate(optimizer, epoch) # 지정된 epoch마다 lr = lr * 0.1 적용
+        adjust_learning_rate(optimizer, epoch) # 지정된 epoch마다 optimizer 속의  lr = lr * 0.1 적용
 
         train(epoch)
         
@@ -224,19 +251,19 @@ if __name__=='__main__':
         1. bin_op.binarization() 실행
 
         binarization()은 아래의 4개 메소드로 구성         
-         self.meancenterConvParams() == 모듈이 Conv2d일때, Conv2d.weight의 평균값을 구함
-         self.clampConvParams() == Conv2d.weight가 -1.0 ~ 1.0 사이의 값이 되도록 조절 
-         self.save_params() == Conv2d.weight저장
-         self.binarizeConvParams() == Conv2d.weight를 -1 또는 1로 binarize
+            self.meancenterConvParams() == Conv2d.weight의 평균값을 구하고, 그 값을 weight 각각에 빼준다.
+            self.clampConvParams() == Conv2d.weight가 -1.0 ~ 1.0 사이의 값이 되도록 조절 
+            self.save_params() == Conv2d.weight저장
+            self.binarizeConvParams() == Conv2d.weight를 -1 또는 1로 binarize
 
 
         2. 순전파
-
-         모델에 data를 전달하여 예상되는 output을 계산한다
+        
+        모델에 data를 전달하여 예상되는 output을 계산한다
 
         3. 역전파
         
-         output과 실제 target에 대한 loss를 구한다
+        output과 실제 target에 대한 loss를 구한다
 
         4. loss가 최소가 되는 방향으로 parameter와 grad 업데이트
 
--- util.py
+++ util.py
@@ -15,13 +15,10 @@ class BinOp():
         start_range = 1
         end_range = count_Conv2d-2
 
-        # <bin_range> 
-        # start_range부터 end_range까지 end_range-start_range+1 개의 균일한 간격의 정수들로 이루어진 array
-        self.bin_range = numpy.linspace(start_range,
-                end_range, end_range-start_range+1)\
-                        .astype('int').tolist()
+        # bin_range --> start_range부터 end_range까지 end_range - start_range + 1개의 균일한 간격으로 이루어진 array
+        self.bin_range = numpy.linspace(start_range, end_range, end_range-start_range+1).astype('int').tolist()
     
-        # num_of_params 는 bin_range의 원소의 개수,  end_range-start_range+1 과 같음
+        # num_of_params 는 bin_range의 원소의 개수,  end_range - start_range + 1 과 같음
         self.num_of_params = len(self.bin_range)
         self.saved_params = []
         self.target_params = []
@@ -35,14 +32,17 @@ class BinOp():
                 index = index + 1
                 # index가 bin_range에 포함된다면
                 if index in self.bin_range:
-                    # Conv2d.weight.data 복사 후 tmp에 저장
-                    # Conv2d.weight.data 와 Conv2d.weight 의 차이 알아보기!
-                    tmp = m.weight.data.clone()
-                    self.saved_params.append(tmp)
-                    self.target_modules.append(m.weight)
+                    tmp = m.weight.data.clone() # Conv2d의 weight값을 복사 후 tmp에 저장
+                    self.saved_params.append(tmp) # saved_params는 nn.Conv2d.weight.data로 구성된 중첩 리스트
+                    self.target_modules.append(m.weight) # target_modules는 nn.Conv2d.weight로 구성된 리스트
+                    
+        # Tensor --> torch.Tensor
+        # nn.Conv2d.weight -->  Tensor 형태의 학습가능한 weight 
+        # nn.Conv2d.bias -->  Tensor 형태의 학습가능한 bias 
+        # nn.Conv2d.weight.data --> array 형태로 weight를 copy해서 반환
 
     def binarization(self):
-        self.meancenterConvParams()  # 모듈이 Conv2d일때, Conv2d.weight의 평균값을 구함
+        self.meancenterConvParams()  # Conv2d.weight 각각에 Conv2d.weight의 평균값을 빼준다. 
         self.clampConvParams() # weight가 -1.0 ~ 1.0 사이의 값이 되도록 조절 
         self.save_params() # Conv2d.weight저장
         self.binarizeConvParams() # Conv2d.weight를 -1 또는 1로 binarize
@@ -50,51 +50,94 @@ class BinOp():
 
 
     def meancenterConvParams(self):
-        for index in range(self.num_of_params):
-            # target_modules는 nn.Conv2d.weight로 구성된 리스트
+        '''
+        target_modules --> nn.Conv2d.weight 전체를 모은 리스트
+        target_modules[idx] --> 특정한 Conv2d.weight
+        target_modules[idx].data -->  torch.Tensor
+        target_modules[idx].data.size() --> tensor의 크기
+        torch.Tensor클래스의 mean(dim = None, keepdim = False) --> input tensor 속 모든 원소들의 평균값을 반환
+        torch.Tensor클래스의 mul(value) --> input tensor에 value를 곱한다
+        torch.Tensor클래스의 expand_as(other) --> tensor의 크기를 other의 크기와 동일하게 확장시킨다
+        torch.Tensor클래스의 add(other) --> tensor 속 모든 원소들에 other를 더한다
+        '''
+        for index in range(self.num_of_params): # num_of_params --> bin_range의 길이
             s = self.target_modules[index].data.size()
-            negMean = self.target_modules[index].data.mean(1, keepdim=True).\
-                    mul(-1).expand_as(self.target_modules[index].data)
+            
+            # negative mean --> weight의 평균을 구한 후 -1곱해서 음수로 만든다.
+            negMean = self.target_modules[index].data.mean(1, keepdim=True).mul(-1).expand_as(self.target_modules[index].data)
+            # weight에 negative mean을 더해준다
             self.target_modules[index].data = self.target_modules[index].data.add(negMean)
 
     def clampConvParams(self):
+        '''
+        torch.Tensor클래스의 clamp(min,max) --> 모든 원소를 min, max 안의 범위로 고정시킨다 
+        '''
         for index in range(self.num_of_params):
-            # target_modules는 nn.Conv2d.weight로 구성된 리스트
-            self.target_modules[index].data = \
-                    self.target_modules[index].data.clamp(-1.0, 1.0)
+            self.target_modules[index].data = self.target_modules[index].data.clamp(-1.0, 1.0)
 
     def save_params(self):
+        '''
+        saved_param 리스트에 target_modules[idx].data를 copy
+        '''
         for index in range(self.num_of_params):
             self.saved_params[index].copy_(self.target_modules[index].data)
 
     def binarizeConvParams(self):
+        '''
+        < torch.Tensor클래스 >
+        nelement() --> num element : input tensor 속 원소의 개수를 반환
+        size() --> tensor의 크기 반환
+        norm() --> tensor의 norm 반환, 벡터의 크기과 동일
+        torch.norm(input, p='fro', dim=None, keepdim=False, out=None, dtype=None)
+        cf) 벡터 v = (1, 2, 3) 이라면 벡터 v의 크기는 root(1+4+9)
+        sum(dim, keepdim = False) --> 지정한 dim의 각 행을 모두 더한 값을 반환. 
+        sign() --> 원소 각각에 sign()함수 적용, 양수는 1, 음수는 -1, 0은 0
+        mul(other) --> 원소 각각에 other를 곱해준다. 
+        div(other) --> 원소 각각에 other를 나눠준다.
+        m.expand(s) --> 
+        
+        n --> torch.Tensor[0] 속의 원소의 개수
+        s --> torch.Tensor의 크기
+        m --> torch.Tensor의 norm을 구한다. --> output_0 : tensor형태 
+            output_0의 2 dim의 각 행의 총 합을 구한다. --> output_1 : tensor형태
+            output_1의 1 dim의 각 행의 총 합을 구한다. --> output_2 : tensor형태
+            output_2를 n으로 나눈다. --> m
+        
+        target_modules[idx].data에 sign()함수를 적용한 후, m을 s의 크기로 확장한 값을 곱해준다. 
+        '''
         for index in range(self.num_of_params):
             n = self.target_modules[index].data[0].nelement()
             s = self.target_modules[index].data.size()
-            m = self.target_modules[index].data.norm(1, 3, keepdim=True)\
-                    .sum(2, keepdim=True).sum(1, keepdim=True).div(n)
-            self.target_modules[index].data = \
-                    self.target_modules[index].data.sign().mul(m.expand(s))
+            m = self.target_modules[index].data.norm(1, 3, keepdim=True).sum(2, keepdim=True).sum(1, keepdim=True).div(n)
+            self.target_modules[index].data = self.target_modules[index].data.sign().mul(m.expand(s))
 
     def restore(self):
+        '''
+        target_modules[idx].data에 saved_param[idx]의 값을 복사해서 저장
+        '''
         for index in range(self.num_of_params):
             self.target_modules[index].data.copy_(self.saved_params[index])
 
     def updateBinaryGradWeight(self):
+        '''
+        n --> weight[0]의 원소의 개수
+        s --> weight의 텐서의 크기
+        m --> weight 텐서의 norm을 구한다. --> output_0 , tensor형태
+            output_0의 2 dim의 각 행의 총합을 구한다 --> output_1, tensor형태
+            output_1의 1 dim의 각 행의 총합을 구한다 --> output_2, tensor형태
+            output_2를 n으로 나누고, s의 크기로 확장한다 --> m
+        '''
         for index in range(self.num_of_params):
             weight = self.target_modules[index].data
             n = weight[0].nelement()
             s = weight.size()
-            m = weight.norm(1, 3, keepdim=True)\
-                    .sum(2, keepdim=True).sum(1, keepdim=True).div(n).expand(s)
-            m[weight.lt(-1.0)] = 0 
-            m[weight.gt(1.0)] = 0
+            m = weight.norm(1, 3, keepdim=True).sum(2, keepdim=True).sum(1, keepdim=True).div(n).expand(s)
+            m[weight.lt(-1.0)] = 0 # weight가 -1보다 작으면 0
+            m[weight.gt(1.0)] = 0 # weight가 1보다 크면 0
             # m = m.add(1.0/n).mul(1.0-1.0/s[1]).mul(n)
-            # self.target_modules[index].grad.data = \
-            #         self.target_modules[index].grad.data.mul(m)
+            # self.target_modules[index].grad.data = self.target_modules[index].grad.data.mul(m)
             m = m.mul(self.target_modules[index].grad.data)
             m_add = weight.sign().mul(self.target_modules[index].grad.data)
-            m_add = m_add.sum(3, keepdim=True)\
-                    .sum(2, keepdim=True).sum(1, keepdim=True).div(n).expand(s)
+            m_add = m_add.sum(3, keepdim=True).sum(2, keepdim=True).sum(1, keepdim=True).div(n).expand(s)
             m_add = m_add.mul(weight.sign())
             self.target_modules[index].grad.data = m.add(m_add).mul(1.0-1.0/s[1]).mul(n)
